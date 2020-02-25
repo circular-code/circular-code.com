eb0cfd10fd41b330d000b9cd3c9679f70ba55031
RocketScience <stephanos@malikotsinas.de>
2020-02-25 21:01:13 +0100

    removed inline script, renamed scroll.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 (function() {
+
+	function waitForRunAnimation() {
+		if (window.runAnimation)
+			runAnimation(document.getElementById('canvas'), 3, 0.5, "rgba(0,0,0,0.5)", 0.1);
+		else
+			setTimeout(function() {
+				waitForRunAnimation();
+			}, 500);
+	}
+
 	window.addEventListener('load', function() {
 		window.setTimeout(function() {
 			document.body.classList.remove('is-preload');
+			waitForRunAnimation();
 		}, 100);
 	});
 })();
